6e5aacb3f6f870aba88b2cf43cde4bc6fed73226
shia14 <blessingsphiri196@gmail.com>
2026-02-13 06:59:04 +0200

    turned it into a android app

---
+++ android/app/src/main/java/com/mykweza/app/DelegationService.java
@@ -0,0 +1,14 @@
+package com.mykweza.app;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
